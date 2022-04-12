34f476518cba0e1d0f6e174e903a36f00bebd5cc
hannakim12 <hanna010510@gmail.com>
2022-04-12 11:14:02 +0900

    임시커밋3

--- src/main/java/com/bookdabang/khn/controller/EventBoardController.java
+++ src/main/java/com/bookdabang/khn/controller/EventBoardController.java
@@ -1,7 +1,10 @@
 package com.bookdabang.khn.controller;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +13,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.bookdabang.common.domain.EventBoardVO;
+import com.bookdabang.common.domain.AttachFileVO;
+import com.bookdabang.common.domain.EventBoardVo;
+import com.bookdabang.common.domain.ReportBoard;
+import com.bookdabang.common.etc.IPCheck;
+import com.bookdabang.khn.etc.UploadFile;
 import com.bookdabang.khn.service.EventService;
 
 @Controller
@@ -23,17 +32,45 @@ public class EventBoardController {
 	@Inject
 	private EventService service;
 	
+	private List<UploadFile> upfileLst = new ArrayList<UploadFile>(); // 업로드된 파일들의 리스트
+	
+	//ETC-------------------------------------------------------------
+	@RequestMapping(value = "/allBestList", method = RequestMethod.GET)
+	public String allBestList(Locale locale, Model model) throws Exception {
+		System.out.println("베스트 게시글을 불러옵니다");
+		List bestBoardList = service.allBestList();
+		model.addAttribute("allBestList", allBestList);
+		
+		return "/event/allBestList";
+	}
+	
+
 	// reading-----------------------------------------------------------
 	// GET방식으로 통신하여 들어오는 이벤트 하위, 전체 리스트를 불러오는 기능을 작성
 	@RequestMapping(value = "/allEventList", method = RequestMethod.GET) 
-	public void allEventList(Locale locale, Model model) throws Exception {
+	public String allEventList(Locale locale, Model model) throws Exception {
 		System.out.println("전체 이벤트 목록을 불러옵니다");
 		List eventBoardList = service.allEventList();
 		model.addAttribute("eventBoardList", eventBoardList);
+		
+		return "/event/allEventList";
 	}
 	
-	@RequestMapping(value = "/readEvent", method = RequestMethod.GET)
-	public void readEvent() {
+	@RequestMapping(value = "/detailEvent", method = RequestMethod.GET)
+	public String readEvent(@RequestParam("boardno") String boardno, Model model, HttpServletRequest request) throws Exception {
+		int no = Integer.parseInt(boardno); //넘겨온 글번호를 int로 변환하여 저장
+		String ipAddr = null; // ip주소를 저장할 변수 생성
+		System.out.println(no + "번 게시글의 상세 페이지를 띄웁니다");
+		
+		ipAddr = IPCheck.getIPAddr(request); // 리퀘스트를 요청한 ip주소를 체크하여 ipaddr에 저장
+		Timestamp lastConnection = service.pageViewCheck(ipAddr, no); // 해당 게시글(no)에 접근한 마지막 시간을 체크
+		
+		
+		
+		EventBoardVo detailEvent = service.readDetailEvent(no);
+		model.addAttribute("detailEvent", detailEvent);
+		
+		return "event/detailEvent";
 		
 	}
 	
@@ -47,9 +84,32 @@ public class EventBoardController {
 	
 	@ResponseBody
 	@RequestMapping(value = "/insertEvent", method = RequestMethod.POST)
-	public String insertEvent(Locale locale, Model model, HttpServletRequest request) throws Exception {
+	public String insertEvent(EventBoardVo newEvent, RedirectAttributes rttr) throws Exception  {
+		String result = "";
+		System.out.println("글을 등록합니다.");
 		
-		return null;
+		if (service.insertEvent(newEvent)) {
+			rttr.addFlashAttribute("result", "success");
+			result = "redirect:/event/allEventList";
+		} else {
+			rttr.addFlashAttribute("result", "fail");
+			result = "redirect:/event/insertEvent";
+		}
+
+		return result; // 기존 페이지로 이동
+		
 	}
 	
+	
+	// delete
+	@RequestMapping(value = "/eventDel")
+	public String deleteEvent(@RequestParam("boardno") String boardno, Model model, HttpServletRequest request) throws Exception {
+		int no = Integer.parseInt(boardno);
+		int result = 0;
+		System.out.println(no + "번 게시글을 삭제합니다");
+		
+		int deleteEvent = service.deleteEvent(no);
+		
+		return "redirect:/event/allEventList";
+	}
 }
